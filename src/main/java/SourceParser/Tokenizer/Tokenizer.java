2dedc02083183b2a9c7abf37b122aa6b23d2401e
cheonHwi <hoba0409@naver.com>
2025-11-14 12:40:10 +0900

    feat(Tokenizer) 코드가 들어오면 코드를 토큰화해서 저장하는 토크나이저 구현
    - peek, isAtEnd : 현재 읽고 있는 구문의 위치 확인
    - nextChar : 현재 구문의 위치를 리턴, 다음 글자로 이동

---
+++ src/main/java/SourceParser/Tokenizer/Tokenizer.java
@@ -0,0 +1,112 @@
+package SourceParser.Tokenizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tokenizer {
+    private String source;
+    private int position;
+    private int line;
+    private int column;
+    private List<Token> tokens;
+
+    public Tokenizer(String source) {
+        this.source = source;
+        this.tokens = new ArrayList<>();
+        this.position = 0;
+        this.line = 1;
+        this.column = 1;
+    }
+
+    public List<Token> tokenize() {
+        while (!isAtEnd()) {
+            tokenizeNext();
+        }
+        tokens.add(new Token(TokenType.EOF, "", line, column));
+        return tokens;
+    }
+
+    private void tokenizeNext() {
+        char c = nextChar();
+
+        switch (c) {
+            case '(': addToken(TokenType.LPAREN, "("); break;
+            case ')': addToken(TokenType.RPAREN, ")"); break;
+            case '{': addToken(TokenType.LBRACE, "{"); break;
+            case '}': addToken(TokenType.RBRACE, "}"); break;
+            case '[': addToken(TokenType.LBRACKET, "["); break;
+            case ']': addToken(TokenType.RBRACKET, "]"); break;
+            case '<': addToken(TokenType.LT, "<"); break;
+            case '>': addToken(TokenType.GT, ">"); break;
+            case ',': addToken(TokenType.COMMA, ","); break;
+            case ';': addToken(TokenType.SEMICOLON, ";"); break;
+            case '.': addToken(TokenType.DOT, "."); break;
+            case ':': addToken(TokenType.COLON, ":"); break;
+            case '+': addToken(TokenType.PLUS, "+"); break;
+            case '-': addToken(TokenType.MINUS, "-"); break;
+            case '*': addToken(TokenType.STAR, "*"); break;
+            case '%': addToken(TokenType.PERCENT, "%"); break;
+            case '=': addToken(TokenType.ASSIGN, "="); break;
+            case '/': addToken(TokenType.SLASH, "/"); break;
+
+            case ' ':
+            case '\r':
+            case '\t':
+                // 공백 무시
+                break;
+
+            case '\n':
+                line++;
+                column = 1;
+                break;
+
+            default:
+                if (isChar(c)) {
+                    tokenizeWord();
+                } else {
+                    addToken(TokenType.UNKNOWN, String.valueOf(c));
+                }
+                break;
+        }
+    }
+
+    private void tokenizeWord() {
+        int start = position - 1;
+        int startColumn = column - 1;
+
+        while (!isAtEnd() && isCharNumber(peek())) {
+            nextChar();
+        }
+
+        String word = source.substring(start, position);
+        TokenType type = KeywordMap.getType(word);
+
+        tokens.add(new Token(type, word, line, startColumn));
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(position);
+    }
+
+    private boolean isAtEnd() {
+        return position >= source.length();
+    }
+
+    private char nextChar() {
+        column++;
+        return source.charAt(position++);
+    }
+
+    private boolean isChar(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isCharNumber(char c) {
+        return isChar(c) || (c >= '0' && c <= '9');
+    }
+
+    private void addToken(TokenType type, String value) {
+        tokens.add(new Token(type, value, line, column-value.length()));
+    }
+}
