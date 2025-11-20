6e39087f2072d69c26306814f385e6b8c250a9d5
cheonHwi <hoba0409@naver.com>
2025-11-20 20:27:56 +0900

    feat(Main): Parser 예제 코드 작성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,16 @@
 import SourceParser.Lexer.Lexer;
-import SourceParser.Tokenizer.Token;
-import SourceParser.Tokenizer.Tokenizer;
-
-import java.util.List;
+import SourceParser.Model.MethodInfo;
+import SourceParser.Parser.Parser;
 
 public class Main {
     public static void main(String[] args) {
-        String code = "public static void main(String[] args) {}";
+        String code = "public List<CustomType<String, double>> main(int[] numList) {}";
 
         Lexer lexer = new Lexer(code);
 
-        while (!lexer.isAtEnd()) {
-            Token token = lexer.getCurrentToken();
-            System.out.println(token);
-            lexer.moveForward();
-        }
+        Parser parser = new Parser(lexer);
+        MethodInfo methodInfo = parser.getMethodInfo();
+
+        System.out.println(methodInfo);
     }
 }
