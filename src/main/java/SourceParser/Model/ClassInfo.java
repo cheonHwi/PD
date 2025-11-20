8720a64ba3daefbd5aa190931fbba45721eb7094
cheonHwi <hoba0409@naver.com>
2025-11-20 23:36:04 +0900

    Feat(ClassInfo): 클래스를 사람이 인식할때 필요할 접근제한자, 상속클래스, 임포트하는 모듈들 Getter/Setter 구현

--- src/main/java/SourceParser/Model/ClassInfo.java
+++ src/main/java/SourceParser/Model/ClassInfo.java
@@ -4,13 +4,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClassInfo {
+    String accessModifier;
+    List<String> modifiers;
     String className;
     String packageName;
+    List<String> imports;
+    String extendsClass;
+    List<String> implementsList;
     List<MethodInfo> methods;
-    List<String> dependencies;  // 이 클래스가 의존하는 다른 클래스들
 
     public ClassInfo() {
+        this.imports = new ArrayList<>();
         this.methods = new ArrayList<>();
-        this.dependencies = new ArrayList<>();
+        this.modifiers = new ArrayList<>();
+        this.implementsList = new ArrayList<>();
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getAccessModifier() {
+        return accessModifier;
+    }
+
+    public void setAccessModifier(String accessModifier) {
+        this.accessModifier = accessModifier;
+    }
+
+    public void setModifiers(List<String> modifiers) {
+        this.modifiers = modifiers;
+    }
+
+    public List<String> getImports() {
+        return imports;
+    }
+
+    public String getExtendsClass() {
+        return extendsClass;
+    }
+
+    public void setExtendsClass(String extendsClass) {
+        this.extendsClass = extendsClass;
+    }
+
+    public List<String> getImplementsList() {
+        return implementsList;
+    }
+
+    public List<MethodInfo> getMethods() {
+        return methods;
     }
 }
