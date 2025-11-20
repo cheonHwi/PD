b16064af3cbf3d7cc9566e9670a839a81bfee734
cheonHwi <hoba0409@naver.com>
2025-11-20 20:28:17 +0900

    feat(Parser): ClassParser 모듈 추가

--- src/main/java/SourceParser/Parser/Parser.java
+++ src/main/java/SourceParser/Parser/Parser.java
@@ -5,9 +5,11 @@ import SourceParser.Model.MethodInfo;
 
 public class Parser {
     private final MethodParser methodParser;
+    private final ClassParser classParser;
 
     public Parser(Lexer lexer) {
         this.methodParser = new MethodParser(lexer);
+        this.classParser = new ClassParser(lexer);
     }
 
     public MethodInfo getMethodInfo() {
